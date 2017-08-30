3aa1fee6badecff46aef55dbf09fd6818689efc9
Mythz91 <mythili1866@gmail.com>
2017-08-29 19:31:45 -0500

    First base proj

---
+++ src/main/java/medicalInsights/application/Application.java
@@ -0,0 +1,22 @@
+package medicalInsights.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("medicalInsights")
+@EntityScan( basePackages = {"medicalInsights.dao"} )
+@EnableJpaRepositories("medicalInsights.repo")
+
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("running");
+
+	}
+
+}
